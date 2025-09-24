f8eef44ed553946d457ef8132e47a1f43488c26e
YashSharma10 <yashsharma67953@gmail.com>
2025-09-24 19:53:52 +0530

    Remove unnecessary comments in Career component import and usage

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,7 +5,7 @@ import About from "../components/About";
 import Services from "../components/Services";
 import WhyChooseUs from "../components/WhyChooseUs";
 import CaseStudies from "../components/CaseStudies";
-import Career from "../components/Career"; // ✅ match the file name
+import Career from "../components/Career"; // 
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -19,7 +19,7 @@ export default function Homepage() {
       <Services />
       <WhyChooseUs />
       <CaseStudies />
-      <Career /> {/* ✅ matches import */}
+      <Career /> 
       <Contact />
       <Footer />
     </>
